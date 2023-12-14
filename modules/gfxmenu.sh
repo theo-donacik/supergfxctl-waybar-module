618ea27bc016b3637b6503f9b105c5d88f1f4231
The0919 <theodonacik@gmail.com>
2023-12-13 23:36:25 -0500

    Fixed syntax error in config and removed compute mode

--- modules/gfxmenu.sh
+++ modules/gfxmenu.sh
@@ -1,22 +1,12 @@
-width=-550
+width=-520
 state=$(supergfxctl -S)
-ans=$(printf "Integrated\nCompute\nHybrid\nDedicated\nVFIO" |wofi --dmenu --width=5 -L 5 --style=$HOME/.config/waybar/modules/wofistyle.css --hide-scroll --location 3 --x $width -p "State: $state")
+ans=$(printf "Integrated\nHybrid" |wofi --dmenu --width=5 -L 3 --style=$HOME/.config/waybar/modules/wofistyle.css --hide-scroll --location 3 --x $width -p "State: $state")
 case $ans in
   Integrated)
-    supergfxctl -m integrated
-    ;;
-
-  Compute)
-    supergfxctl -m compute
+      supergfxctl -m Integrated
     ;;
   Hybrid)
-    supergfxctl -m hybrid && swaynag -t warning -m 'Must logout to complete graphics change' -b 'yes, exit sway' 'swaymsg exit'
-    ;;
-  Dedicated)
-    supergfxctl -m dedicated  && swaynag -t warning -m 'Must logout to complete graphics change' -b 'yes, exit sway' 'swaymsg exit'
-    ;;
-  VFIO)
-    supergfxctl -m vfio
+    supergfxctl -m Hybrid
     ;;
   *)
     echo $ans
@@ -24,3 +14,4 @@ case $ans in
 esac
 
 echo $ans
+
